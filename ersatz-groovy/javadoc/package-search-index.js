e395664f4f3a36ec39fbeb70a01d33310d36ba02
Christopher J. Stehno <chris.stehno@gmail.com>
2022-01-25 12:32:01 -0600

    Generated by gradle-git-publish.

---
+++ ersatz-groovy/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.cjstehno.ersatz"},{"l":"io.github.cjstehno.ersatz.cfg"},{"l":"io.github.cjstehno.ersatz.encdec"},{"l":"io.github.cjstehno.ersatz.match"},{"l":"space.jasan.support.groovy.closure"}];updateSearchResults();
