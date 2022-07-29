6c9efcd0b8ffedd71583069332ea99128b99341c
Christopher J. Stehno <chris.stehno@gmail.com>
2022-07-29 05:43:16 -0500

    Added groovy json encoder/decoder and some added javadocs

---
+++ ersatz-groovy/src/main/java/io/github/cjstehno/ersatz/encdec/JsonEncoder.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2022 Christopher J. Stehno
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.cjstehno.ersatz.encdec;
+
+import groovy.json.JsonOutput;
+
+import java.util.function.Function;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+/**
+ * An encoder based on the Groovy <code>JsonOutput</code> serializer.
+ */
+public class JsonEncoder implements Function<Object, byte[]> {
+
+    /**
+     * A helper method to create an instance of the JsonEncoder.
+     *
+     * @return an instance of the encoder
+     */
+    public static Function<Object, byte[]> jsonEncoder() {
+        return new JsonEncoder();
+    }
+
+    /**
+     * Converts the provided object to an array of bytes containing the JSON representation of the object.
+     *
+     * @param obj the object being encoded
+     * @return the encoded bytes of the object as JSON
+     */
+    @Override public byte[] apply(final Object obj) {
+        return JsonOutput.toJson(obj).getBytes(UTF_8);
+    }
+}
--- ersatz/src/main/java/io/github/cjstehno/ersatz/util/HttpHeaders.java
+++ ersatz/src/main/java/io/github/cjstehno/ersatz/util/HttpHeaders.java
@@ -20,23 +20,98 @@ package io.github.cjstehno.ersatz.util;
  */
 public interface HttpHeaders {
 
+    /**
+     * Authorization HTTP Header.
+     */
     String AUTHORIZATION = "Authorization";
+
+    /**
+     * Cache-Control HTTP Header.
+     */
     String CACHE_CONTROL = "Cache-Control";
+
+    /**
+     * Expires HTTP Header.
+     */
     String EXPIRES = "Expires";
+
+    /**
+     * Pragma HTTP Header.
+     */
     String PRAGMA = "Pragma";
+
+    /**
+     * Keep-Alive HTTP Header.
+     */
     String KEEP_ALIVE = "Keep-Alive";
+
+    /**
+     * Accept-Encoding HTTP Header.
+     */
     String ACCEPT_ENCODING = "Accept-Encoding";
+
+    /**
+     * Cookie HTTP Header.
+     */
     String COOKIE = "Cookie";
+
+    /**
+     * Set-Cookie HTTP Header.
+     */
     String SET_COOKIE = "Set-Cookie";
+
+    /**
+     * Content-Disposition HTTP Header.
+     */
     String CONTENT_DISPOSITION = "Content-Disposition";
+
+    /**
+     * Content-Length HTTP Header.
+     */
     String CONTENT_LENGTH = "Content-Length";
+
+    /**
+     * Content-Type HTTP Header.
+     */
     String CONTENT_TYPE = "Content-Type";
+
+    /**
+     * Content-Encoding HTTP Header.
+     */
     String CONTENT_ENCODING = "Content-Encoding";
+
+    /**
+     * Location HTTP Header.
+     */
     String LOCATION = "Location";
+
+    /**
+     * Host HTTP Header.
+     */
     String HOST = "Host";
+
+    /**
+     * Referer HTTP Header.
+     */
     String REFERER = "Referer";
+
+    /**
+     * User-Agent HTTP Header.
+     */
     String USER_AGENT = "User-Agent";
+
+    /**
+     * Allow HTTP Header.
+     */
     String ALLOW = "Allow";
+
+    /**
+     * Transfer-Encoding HTTP Header.
+     */
     String TRANSFER_ENCODING = "Transfer-Encoding";
+
+    /**
+     * Date HTTP Header.
+     */
     String DATE = "Date";
 }
